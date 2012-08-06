f1b68c5a507934dde3ed47c416b9d6dc074d34cd
Christian Gmeiner <christian.gmeiner@gmail.com>
2012-08-06 14:11:04 +0200

    get it compiling with warnings

--- modules/em8300_params.c
+++ modules/em8300_params.c
@@ -18,6 +18,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <linux/moduleparam.h>
 #include "em8300_params.h"
 #include <linux/em8300.h>
 #include "em8300_driver.h"
